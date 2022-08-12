121d5dd98dc9a229d66efcc48be6a7b848c26307
dahoumanekhalil <k.mr17@yahoo.com>
2022-08-12 11:02:19 +0100

    sidebar version 2

--- main.js
+++ main.js
@@ -1,15 +1,7 @@
 let sidebar = document.querySelector('.sidebar');
-let toggle = document.querySelector('#toggleMode')
-// let sun = document.querySelector('')
-let sun = document.querySelector('.pageMode .fa-sun')
-let moon = document.querySelector('.pageMode .fa-moon')
-let mode ;
 
-mode = localStorage.getItem('mode');
-document.body.className = mode
 sidebar.addEventListener('mouseover', () => {
     document.querySelector('.sidebar').style.cssText = `width: 250px;`
-    // document.querySelector('.sidebarTop').style.cssText = `justify-content: center;`;
     document.querySelector('.sidebarTop div').style = `    
     width: 120px;
     transition: 0.2s ease 0.1s;
@@ -45,12 +37,4 @@ sidebar.addEventListener('mouseout', () => {
     width: 0;
     opacity: 0;
     `;
-})
-
-toggle.addEventListener('change', () => {
-    mode = document.body.className = document.body.className == `dark` ? `light` : `dark`
-    localStorage.setItem('mode', mode)
-    console.log(localStorage.getItem('mode'))
-    sun.className = sun.className == `fa-solid fa-sun` ? `fa-regular fa-sun` : `fa-solid fa-sun`;
-    moon.className = moon.className == `fa-solid fa-moon` ? `fa-regular fa-moon` : `fa-solid fa-moon`;
 })
